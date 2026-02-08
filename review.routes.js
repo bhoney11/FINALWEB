1084248e10a82a01f0bac6f2b025c7461392b742
bhoney11 <tebejopa7@gmail.com>
2026-02-08 22:19:43 +0500

    Create review.routes.js

---
+++ review.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const reviewController = require('../controllers/reviewController');
+
+router.get('/:toyId', reviewController.getReviewsByToy);
+
+router.post('/', reviewController.addReview);
+
+module.exports = router;
